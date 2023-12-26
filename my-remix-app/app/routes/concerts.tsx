929c4d23105ffe570a6cde94b32cfd06109ce03d
OumouDi <124316387+OumouDi@users.noreply.github.com>
2023-12-26 10:17:29 +0100

    concerts and musicians routes creation

---
+++ my-remix-app/app/routes/concerts.tsx
@@ -0,0 +1,10 @@
+
+
+export default function ConcertList() {
+    return (
+      <div>
+        <h1>Liste des concerts</h1>
+      
+      </div>
+    );
+  }
---
+++ my-remix-app/app/routes/musicians.tsx
@@ -0,0 +1,11 @@
+
+
+export default function MusicianList() {
+    return (
+      <div>
+        <h1>Profils de musiciens</h1>
+      
+      </div>
+    );
+  }
+  
