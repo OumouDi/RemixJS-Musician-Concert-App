edfc69791272ab2128f3d5e1ec4dc46966296aa2
OumouDi <124316387+OumouDi@users.noreply.github.com>
2023-12-26 10:49:59 +0100

    get all musiciens

---
+++ my-remix-app/app/models/musicien.server.ts
@@ -0,0 +1,26 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const DB_FILE_PATH = path.join(process.cwd(), 'db.json');
+
+export type Musicien = {
+  id: number;
+  name: string;
+  instrument: Array<string> | string;
+  style: Array<string> | string;
+};
+
+
+export async function getMusicians(): Promise<Musicien[]> {
+  try {
+    console.log('Chargement des musiciens depuis le fichier JSON...');
+    const data = await fs.readFile(DB_FILE_PATH, 'utf-8');
+    const jsonData = JSON.parse(data);
+
+    return jsonData.musiciens || [];
+  } catch (error) {
+    console.error('Erreur lors de la lecture des musiciens depuis le fichier JSON.', error);
+    return [];
+  }
+}
+
--- my-remix-app/app/routes/musicians.tsx
+++ my-remix-app/app/routes/musicians.tsx
@@ -1,11 +1,35 @@
+import Navbar from '../components/Navbar';
+import { json } from '@remix-run/node';
+import { useLoaderData , Link } from '@remix-run/react';
+import { getMusicians } from '../models/musicien.server';
 
 
-export default function MusicianList() {
+export const loader = async () => {
+  console.log('loader')
+  return json({ musiciens: await getMusicians() });
+};
+
+
+function MusicianList() {
+    const { musiciens } = useLoaderData<typeof loader>();
+    console.log('Musiciens dans le composant:', musiciens);
+  
+    
     return (
       <div>
-        <h1>Profils de musiciens</h1>
-      
+        <Navbar />
+        <h1>Profil de musiciens</h1>
+        <ul>
+          {musiciens.map((music) => (
+            <li key={music.id}>
+              <Link to={`/musicians/${music.id}`}>
+                {music.name} - {music.style} - {music.instrument}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
-  
+
+    export default MusicianList;
