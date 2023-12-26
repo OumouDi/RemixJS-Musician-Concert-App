8d32b7e77f9fbe48694687693c0c7ce70a219950
OumouDi <124316387+OumouDi@users.noreply.github.com>
2023-12-26 11:00:07 +0100

    musician component decomposition

---
+++ my-remix-app/app/components/MusicianCard.jsx
@@ -0,0 +1,48 @@
+// MusicienCard.js
+
+import PropTypes from 'prop-types'; 
+import { Link } from '@remix-run/react';
+
+function MusicianCard({ music }) {
+  return (
+    <div
+      className="bg-white p-6 rounded-lg shadow-md text-center transform transition-transform hover:scale-105 focus:outline-none focus:ring focus:border-blue-300"
+    >
+      <h2 className="text-xl font-bold mb-2 mb-4">{music.name}</h2>
+      <div className="mb-2">
+        {Array.isArray(music.style) ? (
+          music.style.map((style, index) => (
+            <span
+              key={index}
+              className="bg-black text-white px-2 py-1 rounded-md inline-block mr-2 mb-2"
+            >
+              {style}
+            </span>
+          ))
+        ) : (
+          <span className="bg-black text-white px-2 py-1 rounded-md">
+            {music.style}
+          </span>
+        )}
+      </div>
+      <p className="text-gray-700">
+        <strong>Instruments:</strong>{' '}
+        {Array.isArray(music.instrument) ? music.instrument.join(', ') : music.instrument}
+      </p>
+      <Link to={`/musicien/${music.id}`} className="text-white bg-blue-500 mt-4 block p-2 rounded-md hover:bg-blue-600">
+        Voir le profil
+      </Link>
+    </div>
+  );
+}
+
+MusicianCard.propTypes = {
+  music: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    style: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]).isRequired,
+    instrument: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]).isRequired,
+  }).isRequired,
+};
+
+export default MusicianCard;
--- my-remix-app/app/routes/musicians.tsx
+++ my-remix-app/app/routes/musicians.tsx
@@ -1,7 +1,8 @@
 import Navbar from '../components/Navbar';
 import { json } from '@remix-run/node';
-import { useLoaderData, Link } from '@remix-run/react';
+import { useLoaderData } from '@remix-run/react';
 import { getMusicians } from '../models/musicien.server';
+import MusicianCard from '../components/MusicianCard';
 import musicienBackgrundImage from '../../public/images/background.jpg';
 
 export const loader = async () => {
@@ -9,34 +10,14 @@ export const loader = async () => {
 };
 
 function MusicianList() {
-   const { musiciens } = useLoaderData<typeof loader>();
+  const { musiciens } = useLoaderData<typeof loader>();
 
   return (
     <div className="flex flex-col items-center min-h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${musicienBackgrundImage })` }}>
       <Navbar />
       <h1 className="text-3xl font-bold mt-8 mb-8 text-white">Liste des musiciens</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {musiciens.map((music) => (
-          <div
-            key={music.id}
-            className="bg-gradient-to-r from-blue-200 to-blue-300 p-6 rounded-lg shadow-md text-center transform transition-transform hover:scale-105 focus:outline-none focus:ring focus:border-blue-300"
-          >
-            <h2 className="text-xl font-bold mb-2">{music.name}</h2>
-            <p className="text-gray-700 mb-2">
-              <strong>Styles:</strong> {Array.isArray(music.style) ? music.style.join(', ') : music.style}
-            </p>
-            <p className="text-gray-700">
-              <strong>Instruments:</strong>{' '}
-              {Array.isArray(music.instrument) ? music.instrument.join(', ') : music.instrument}
-            </p>
-            <Link
-              to={`/musicien/${music.id}`}
-              className="text-white bg-blue-500 mt-4 block p-2 rounded-md hover:bg-blue-600"
-            >
-              Voir le profil
-            </Link>
-          </div>
-        ))}
+        {musiciens.map((music) => <MusicianCard key={music.id} music={music} />)}
       </div>
     </div>
   );
